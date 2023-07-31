7a02f55b2360d074dcc57423a8b37688a34e96ce
Yetunde Alese <yaalese1@gmail.com>
2023-07-31 17:12:03 -0400

    completed 7/31/23

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    return [n for n in num_list if n % 2 == 0 ]
     pass
 
 def make_exclamation(sentence_list):
+    return [n + "!" for n in sentence_list ]
     pass
